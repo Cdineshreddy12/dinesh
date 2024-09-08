b516827caa896367ce5123b4d12c0b888c58f6d8
dinesh <yreddycdinesh41@gmail.com>
2024-09-08 17:01:06 +0530

    changes made in the admin root layout

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -3,6 +3,7 @@ import AdminHeader from "./AdminHeader";
 import { Outlet } from "react-router-dom";
 export default function AdminDashboard()
 {
+    // this is admin root layout
     return(
          <>
             <AdminHeader/>
